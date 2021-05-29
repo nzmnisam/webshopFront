5c0219fdaa5423a170a8714f225d5f9b53ec9154
nzmnisam <milostaptap@gmail.com>
2021-05-29 12:26:43 +0200

    dodata navigacija

---
+++ src/Components/Navbar/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+  {
+    title: "Akcija",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "Proizvodi",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "O nama",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "Prijavi se",
+    url: "#",
+    cName: "nav-links-mobile",
+  },
+];
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import Button from "../Buttons/Button";
+import { MenuItems } from "./MenuItems";
+import "./Navbar.css";
+
+const Navbar = () => {
+  const [clicked, setClicked] = useState(false);
+
+  const handleClick = () => {
+    setClicked(!clicked);
+  };
+
+  return (
+    <nav className="Navbar">
+      <h1 className="navbar-logo">
+        WebShop <i className="fab fa-react"></i>
+      </h1>
+      <div className="menu-icon" onClick={handleClick}>
+        <i className={clicked ? "fas fa-times" : "fas fa-bars"}></i>
+      </div>
+      <ul className={clicked ? "nav-menu active" : "nav-menu"}>
+        {MenuItems.map((item, index) => {
+          return (
+            <li key={index}>
+              <a className={item.cName} href={item.url}>
+                {item.title}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <Button>Prijavi se</Button>
+    </nav>
+  );
+};
+
+export default Navbar;
